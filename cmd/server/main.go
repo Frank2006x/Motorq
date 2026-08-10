bbb6168af0501d5685b898a5769e60613e9f6dcb
Frank2006x <frank2006x@gmail.com>
2026-08-10 18:13:25 +0000

    init db pool

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"Frank2006xmotorq/internal/config"
+	"Frank2006xmotorq/internal/db"
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v3"
 )
 
-func main(){
-	app:=fiber.New()
+func main() {
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
+		fmt.Printf("failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	pool, err := db.NewDB(cfg.POSTGRES_DB)
+	if err != nil {
+		fmt.Printf("failed to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	fmt.Println("Connected to database!")
+
+	app := fiber.New()
 	app.Get("/ping", func(c fiber.Ctx) error {
 		return c.SendString("pong")
 	})
-	app.Listen(":8080")
 
+	app.Listen(":8080")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,60 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	POSTGRES_DB          string `mapstructure:"POSTGRES_DB"`
+
+}
+
+func LoadConfig(path string) (Config, error) {
+	configPath, err := findConfigPath(path, ".env")
+	if err != nil {
+		return Config{}, err
+	}
+
+	viper.SetConfigFile(configPath)
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+	var config Config
+
+	err = viper.Unmarshal(&config)
+
+	return config, err
+
+}
+
+func findConfigPath(startDir, fileName string) (string, error) {
+	currentDir, err := filepath.Abs(startDir)
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		candidate := filepath.Join(currentDir, fileName)
+		if _, statErr := os.Stat(candidate); statErr == nil {
+			return candidate, nil
+		} else if !os.IsNotExist(statErr) {
+			return "", statErr
+		}
+
+		parentDir := filepath.Dir(currentDir)
+		if parentDir == currentDir {
+			break
+		}
+		currentDir = parentDir
+	}
+
+	return "", fmt.Errorf("config file %q not found starting from %q", fileName, startDir)
+}
